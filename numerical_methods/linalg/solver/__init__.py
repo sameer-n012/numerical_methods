b3ffbfe52720d37247935c269ab672567435684c
sameer-n012 <sameer.narendran@gmail.com>
2023-05-17 20:41:49 -0400

    added gmres file

--- numerical_methods/linalg/solver/__init__.py
+++ numerical_methods/linalg/solver/__init__.py
@@ -1,5 +1,6 @@
 from .trisolve_methods import trisolve
 from .qrsolve_methods import qrsolve
 from .lusolve_methods import lusolve
+from .iterative_methods import gmres
 
-__all__ = ["trisolve", "qrsolve", "lusolve"]
+__all__ = ["trisolve", "qrsolve", "lusolve", "gmres"]
---
+++ numerical_methods/linalg/solver/iterative_methods.py
@@ -0,0 +1,4 @@
+from numpy import matmul, ndarray
+
+def gmres(A: ndarray, b: ndarray, verbose: bool = False) -> ndarray:
+    raise NotImplementedError()
